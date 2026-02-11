46aff441fc0afa31493a181e734ec5171a1e6055
snow6692 <ahmedha258258@gmail.com>
2026-02-11 14:14:35 +0200

    fix test

---
+++ test.test.ts
@@ -0,0 +1,5 @@
+import { expect, it, test } from "bun:test";
+
+it("basic math works", () => {
+  expect(1 + 1).toBe(2);
+});
--- test.ts
+++
@@ -1,4 +0,0 @@
-console.log("Starting test");
-setTimeout(() => console.log("Waiting 3s"), 3000);
-
-console.log("test completed");
