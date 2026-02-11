9acd32951cdc5c5c5f673853a2e646baebb47322
snow6692 <ahmedha258258@gmail.com>
2026-02-11 14:17:02 +0200

    change the test

---
+++ index.test.ts
@@ -0,0 +1,7 @@
+import { expect, it, describe } from "bun:test";
+
+describe("math utils", () => {
+  it("Should return 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
--- test.test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, it, test } from "bun:test";
-
-it("basic math works", () => {
-  expect(1 + 1).toBe(2);
-});
